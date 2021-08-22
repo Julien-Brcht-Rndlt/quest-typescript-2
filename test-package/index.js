07584a9bdc87a4d9a5b881236549183765d37bb1
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-08-22 16:50:11 +0200

    saving quest#2

---
+++ test-package/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var common_1 = require("@wcs-jn/common");
+console.log(common_1.helloWilder({ name: "Jane", city: "London", email: 'jane@gmail.com' }));
---
+++ test-package/index.ts
@@ -0,0 +1,3 @@
+import { helloWilder } from '@wcs-jn/common';
+
+console.log(helloWilder({ name: "Jane", city: "London", email: 'jane@gmail.com' }));
